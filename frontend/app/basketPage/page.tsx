c1c42d6878ca5ed46f3e2cf2aa94dec079ea6d43
MateuMiralles <mateumiralles714@gmail.com>
2023-09-13 16:55:03 +0200

    Pagines plantilla per a la cesta i usuari/login.

---
+++ frontend/app/basketPage/page.tsx
@@ -0,0 +1,10 @@
+export default function basketPage(){
+return(
+    <main className="">
+      <div className="w-full min-h-screen flex flex-col justify-center">
+       <p className="text-center w-full h-full">Pàgina per a la cesta de la compra</p>
+      </div>
+    </main>
+);
+
+}
---
+++ frontend/app/usersPage/page.tsx
@@ -0,0 +1,10 @@
+export default function basketPage(){
+    return(
+        <main className="">
+          <div className="w-full min-h-screen flex flex-col justify-center">
+           <p className="text-center w-full h-full">Pàgina per al LogIn i Usuaris</p>
+          </div>
+        </main>
+    );
+    
+    }
