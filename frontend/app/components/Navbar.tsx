95dae511f02b46089c6bce9922708d72a586a610
MateuMiralles <mateumiralles714@gmail.com>
2023-09-13 04:39:55 +0200

    Navbar Action Icons buttons

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
+import NavbarIconButton from "./NavbarIconButton";
 import NavbarTile from "./NavbarTile";
-
 export default function Navbar() {
   return (
     <div className="group flex flex-row items-center pt-4 hover:bg-slate-100 transition  hover:-translate-y-3  duration-300">
-      <div className="px-4 sm:px-6 lg:px8 flex h-16 items-center ">
+      <div className="flex-1 px-4 sm:px-6 lg:px8 flex h-16 items-center ">
         <Link href="/mainPage" className="ml-4 flex lg:ml-0 gap-x-2">
           <p className="font-bold text-xl">PULLANDBEAR</p>
         </Link>
-      </div> 
-      <NavbarTile title="Sneakers" link="/Sneakers"/>
-      <NavbarTile title="Sweatshirt" link="/Sweatshirts"/>
-      <NavbarTile title="Trousers" link="/Trousers"/>
-      
-      
-      
+
+        <NavbarTile title="Sneakers" link="/Sneakers" />
+        <NavbarTile title="Sweatshirt" link="/Sweatshirts" />
+        <NavbarTile title="Trousers" link="/Trousers" />
+      </div>
+      <div className="flex-4 flex flex-row mr-14 gap-5 ">
+        <NavbarIconButton svgName="/shoppingBagIcon.svg" />
+        <NavbarIconButton svgName="/userIcon.svg" />
+      </div>
     </div>
   );
 }
---
+++ frontend/app/components/NavbarIconButton.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+interface svgProps {
+    svgName: string;
+    
+  }
+
+export default function NavbarIconButton({svgName}:svgProps) {
+  return (
+    <button className="group w-7 h-7 flex items-center justify-center rounded-full  hover:border  hover:border-black hover:scale-125 transition duration-200">
+      <Image
+              src={svgName}
+              
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={20}
+              height={20}
+              priority
+            />
+     
+    </button>
+  );
+}
