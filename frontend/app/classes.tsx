dc8c46d28c536f036526682c7df6fddcfea4ff2e
Idyos <javi.gauxachs@gmail.com>
2023-09-15 21:46:43 +0200

    classe reduida

--- frontend/app/classes.tsx
+++ frontend/app/classes.tsx
@@ -2,7 +2,6 @@ export class Article {
     code: string;
     name: string;
     description: string;
-    sellingAttributes: any[]; // Puedes reemplazar 'any[]' con un tipo específico si es necesario
     color: {
       code: string;
       text: string;
@@ -11,55 +10,8 @@ export class Article {
     whitePrice: {
       price: number;
       currency: string;
-      referenceFlag: boolean;
-      startDate: number;
-      endDate: number;
     };
-    redPrice: {
-      price: number;
-      currency: string;
-      referenceFlag: boolean;
-      startDate: number;
-      endDate: number;
-    };
-    colourDescription: string;
-    pattern: string;
-    lengthCollection: Array<any>;
-    showPriceMarker: boolean;
-    percentageDiscount: string;
-    disclaimer: Array<any>;
-    lightNeeds: Array<any>;
-    wateringNeeds: Array<any>;
-    plantType: Array<any>;
-    plantAndFlowerStyle: Array<any>;
-    salesMode: Array<any>;
-    seats: Array<any>;
-    stuffingMaterial: Array<any>;
-    firmness: Array<any>;
     galleryDetails: Array<any>;
-    fabricSwatchThumbnails: {
-      id: string;
-      mimeType: string;
-      baseUrl: string;
-      url: string;
-      assetType: string;
-    }[];
-    styleWith: {
-      code: string;
-      lengthCollection: any[];
-      showPriceMarker: boolean;
-      styleWithOrigin: string;
-      parentProductCode: string;
-      inStore: boolean;
-      productTransparencyEnabled: boolean;
-      suppliersDetailEnabled: boolean;
-      comingSoon: boolean;
-      external: boolean;
-      palette: boolean;
-      travelSize: boolean;
-      semiBulky: boolean;
-    }[];
-    functions: any[];
     careInstructions: string[];
     compositions: {
       materials: {
@@ -72,89 +24,26 @@ export class Article {
       name: string;
       description: string;
     }[];
-    graphicalAppearanceDesc: string;
-    genericDescription: string;
     variantsList: {
       code: string;
       size: {
-        sizeCode: string;
         name: string;
-        sizeScaleCode: string;
-        sizeScaleDescription: string;
-        sizeOrder: number;
-        sizeFilter: string;
-        market: string;
       };
     }[];
-    concepts: string[];
-    parentProductCode: string;
-    styleWithScenario: string;
-    inStore: boolean;
-    productTransparencyEnabled: boolean;
-    suppliersDetailEnabled: boolean;
-    suppliersSectionDisabledReason: string;
-    comingSoon: boolean;
-    external: boolean;
-    articleType: string;
-    brandName: string;
-    palette: boolean;
-    travelSize: boolean;
-    corporateBrandId: number;
-    woodCompositions: any[];
     articleCountryOfProduction: string;
-    semiBulky: boolean;
-    availabilityOnlineDate: number;
 
     constructor(data: any = {}) {
       this.code = data.code;
       this.name = data.name;
       this.description = data.description;
-      this.sellingAttributes = data.sellingAttributes || [];
       this.color = data.color;
       this.whitePrice = data.whitePrice;
-      this.redPrice = data.redPrice;
-      this.colourDescription = data.colourDescription;
-      this.pattern = data.pattern;
-      this.lengthCollection = data.lelengthCollection;
-      this.showPriceMarker = data.showPriceMarker;
-      this.percentageDiscount = data.percentageDiscount;
-      this.disclaimer = data.disclaimer;
-      this.lightNeeds = data.lightNeeds;
-      this.wateringNeeds = data.wateringNeeds;
-      this.plantType = data.plantType;
-      this.plantAndFlowerStyle = data.plantAndFlowerStyle;
-      this.salesMode = data.salesMode;
-      this.seats = data.seats;
-      this.stuffingMaterial = data.stuffingMaterial;
-      this.firmness = data.firmness;
       this.galleryDetails = data.galleryDetails;
-      this.fabricSwatchThumbnails = data.fabricSwatchThumbnails || [];
-      this.styleWith = data.styleWith || [];
-      this.functions = data.functions || [];
       this.careInstructions = data.careInstructions || [];
       this.compositions = data.compositions || [];
       this.materialDetails = data.materialDetails || [];
-      this.graphicalAppearanceDesc = data.graphicalAppearanceDesc || '';
-      this.genericDescription = data.genericDescription || '';
       this.variantsList = data.variantsList || [];
-      this.concepts = data.concepts || [];
-      this.parentProductCode = data.parentProductCode || '';
-      this.styleWithScenario = data.styleWithScenario || '';
-      this.inStore = data.inStore || false;
-      this.productTransparencyEnabled = data.productTransparencyEnabled || false;
-      this.suppliersDetailEnabled = data.suppliersDetailEnabled || false;
-      this.suppliersSectionDisabledReason = data.suppliersSectionDisabledReason || '';
-      this.comingSoon = data.comingSoon || false;
-      this.external = data.external || false;
-      this.articleType = data.articleType || '';
-      this.brandName = data.brandName || '';
-      this.palette = data.palette || false;
-      this.travelSize = data.travelSize || false;
-      this.corporateBrandId = data.corporateBrandId || 0;
-      this.woodCompositions = data.woodCompositions || [];
       this.articleCountryOfProduction = data.articleCountryOfProduction || '';
-      this.semiBulky = data.semiBulky || false;
-      this.availabilityOnlineDate = data.availabilityOnlineDate || 0;
     }
   }
 
