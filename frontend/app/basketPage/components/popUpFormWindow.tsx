510d9c80f7aeea1a685a6c7344386c2d25962aed
MateuMiralles <mateumiralles714@gmail.com>
2023-09-26 12:52:08 +0200

    -PopUpForm Window style & e.stopPropagation

--- frontend/app/basketPage/components/popUpFormWindow.tsx
+++ frontend/app/basketPage/components/popUpFormWindow.tsx
@@ -1,5 +1,3 @@
-import axios from "axios";
-import { useState } from "react";
 
 type PopUpFormWindowProps = {
   bool: boolean;
@@ -14,12 +12,17 @@ export default function PopUpFormWindow({
 }: PopUpFormWindowProps) {
   return (
     <div
-      style={bool ? { display: "block" } : { display: "none" }}
-      className="absolute left-[50%] top-[50%] z-10 h-[50%] w-[50%] translate-x-[-50%] translate-y-[-50%] rounded border border-black bg-white"
-    >
-      {content}
+    style={{backgroundColor:"rgba(0, 0, 0, 0.7)"}}
+     className={`${bool ? "block":"hidden"} absolute w-full h-full z-20 top-0 cursor-pointer`}
+    onClick={() => setBool(false)}>
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="relative left-[50%] top-[50%] h-[50%] w-[60%] translate-x-[-50%] translate-y-[-50%] rounded border border-black bg-gray-300 cursor-auto"
+      >
+        {content}
 
-      <p onClick={() => setBool(false)}>X</p>
+        <p onClick={() => setBool(false)}>X</p>
+      </div>
     </div>
   );
 }
