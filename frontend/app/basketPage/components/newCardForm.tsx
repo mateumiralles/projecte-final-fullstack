d9a4e76f2c48df44571b10000ff0f8eddb21c911
MateuMiralles <mateumiralles714@gmail.com>
2023-09-26 06:49:30 +0200

    -Limpieza processPuchase amb arxiu propi per newCardForm

---
+++ frontend/app/basketPage/components/newCardForm.tsx
@@ -0,0 +1,113 @@
+import axios from "axios";
+import { useState } from "react";
+
+type NewCardFormProps = {
+  createNewPay: boolean;
+  setCreateNewPay: React.Dispatch<React.SetStateAction<boolean>>;
+  getPaymentMethods: () => Promise<void>;
+};
+
+export default function NewCardForm({
+  createNewPay,
+  setCreateNewPay,
+  getPaymentMethods,
+}: NewCardFormProps) {
+  const [newPaymentMethod, setNewPaymentMethod] = useState<any>({
+    cardNumber: "",
+    expirationDate: "",
+    ownerName: "",
+  });
+
+  const createPaymentMethod = async () => {
+    const user = JSON.parse(localStorage.getItem("user")!);
+    console.log({
+      isDefault: true,
+      type: 1,
+      cardNumber: newPaymentMethod.cardNumber,
+      expirationDate: newPaymentMethod.expirationDate,
+      ownerName: newPaymentMethod.ownerName,
+      userId: 2,
+    });
+    try {
+      const response = await axios.post(
+        `http://localhost:3333/api/paymentMethods/`,
+        {
+          isDefault: true,
+          type: 1,
+          cardNumber: newPaymentMethod.cardNumber,
+          expirationDate: newPaymentMethod.expirationDate,
+          ownerName: newPaymentMethod.ownerName,
+          userId: 1,
+        },
+      );
+      console.log(response);
+      if (response.status === 201) {
+        setCreateNewPay(false);
+        getPaymentMethods();
+      }
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
+  const handleCardNumberChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const inputNumero = event.target.value.replace(/\s/g, ""); // Elimina espacios en blanco actuales
+    let formattedNumero = "";
+
+    for (let i = 0; i < inputNumero.length; i++) {
+      if (i > 0 && i % 4 === 0) {
+        formattedNumero += " "; // Añade un espacio cada 4 caracteres
+      }
+      formattedNumero += inputNumero[i];
+    }
+    setNewPaymentMethod({
+      ...newPaymentMethod,
+      ["cardNumber"]: formattedNumero,
+    });
+  };
+
+  const handleInputChangeCard = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const { name, value } = event.target;
+    setNewPaymentMethod({
+      ...newPaymentMethod,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div
+      style={createNewPay ? { display: "block" } : { display: "none" }}
+      className="absolute left-[50%] top-[50%] z-10 h-[50%] w-[50%] translate-x-[-50%] translate-y-[-50%] rounded border border-black bg-white"
+    >
+      <p>Add a new payment method</p>
+      <input
+        type="text"
+        name="cardNumber"
+        placeholder="Card Number"
+        value={newPaymentMethod.cardNumber}
+        onChange={handleCardNumberChange}
+        maxLength={19}
+      />
+      <input
+        type="text"
+        name="expirationDate"
+        placeholder="Expiration Date"
+        value={newPaymentMethod.expirationDate}
+        onChange={handleInputChangeCard}
+      />
+      <input
+        type="text"
+        name="ownerName"
+        placeholder="Owner Name"
+        value={newPaymentMethod.ownerName}
+        onChange={handleInputChangeCard}
+      />
+      <button onClick={createPaymentMethod}>Add payment method</button>
+      <p onClick={() => setCreateNewPay(false)}>X</p>
+    </div>
+  );
+}
--- frontend/app/basketPage/processPurhcase.tsx
+++ frontend/app/basketPage/processPurhcase.tsx
@@ -5,45 +5,13 @@ import { useEffect, useState } from "react";
 import PaymentMethodSlider from "./components/paymentMethodSlider";
 import AdressSlider from "./components/adressSlider";
 import NewAdressFrom from "./components/newAdressForm";
+import NewCardForm from "./components/newCardForm";
 export default function ProcessPurchase(props: { purchaseSteps: number }) {
   //const [user, setUser] = useState<>();
   const [paymentMethods, setPaymentMethods] = useState<any[]>([]);
   const [address, setAddress] = useState<string>();
   const [createNewPay, setCreateNewPay] = useState<boolean>(false);
   const [createNewAddress, setCreateNewAddress] = useState<boolean>(false);
-  const [newPaymentMethod, setNewPaymentMethod] = useState<any>({
-    cardNumber: "",
-    expirationDate: "",
-    ownerName: "",
-  });
-
-  const handleCardNumberChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const inputNumero = event.target.value.replace(/\s/g, ""); // Elimina espacios en blanco actuales
-    let formattedNumero = "";
-
-    for (let i = 0; i < inputNumero.length; i++) {
-      if (i > 0 && i % 4 === 0) {
-        formattedNumero += " "; // Añade un espacio cada 4 caracteres
-      }
-      formattedNumero += inputNumero[i];
-    }
-    setNewPaymentMethod({
-      ...newPaymentMethod,
-      ["cardNumber"]: formattedNumero,
-    });
-  };
-
-  const handleInputChangeCard = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const { name, value } = event.target;
-    setNewPaymentMethod({
-      ...newPaymentMethod,
-      [name]: value,
-    });
-  };
 
   const getAddress = async () => {
     const user = JSON.parse(localStorage.getItem("user")!);
@@ -76,38 +44,6 @@ export default function ProcessPurchase(props: { purchaseSteps: number }) {
     }
   };
 
-  const createPaymentMethod = async () => {
-    const user = JSON.parse(localStorage.getItem("user")!);
-    console.log({
-      isDefault: true,
-      type: 1,
-      cardNumber: newPaymentMethod.cardNumber,
-      expirationDate: newPaymentMethod.expirationDate,
-      ownerName: newPaymentMethod.ownerName,
-      userId: 2,
-    });
-    try {
-      const response = await axios.post(
-        `http://localhost:3333/api/paymentMethods/`,
-        {
-          isDefault: true,
-          type: 1,
-          cardNumber: newPaymentMethod.cardNumber,
-          expirationDate: newPaymentMethod.expirationDate,
-          ownerName: newPaymentMethod.ownerName,
-          userId: 1,
-        },
-      );
-      console.log(response);
-      if (response.status === 201) {
-        setCreateNewPay(false);
-        getPaymentMethods();
-      }
-    } catch (error: any) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
     console.log("PROCESS PURCHASE");
     console.log(props.purchaseSteps);
@@ -128,39 +64,18 @@ export default function ProcessPurchase(props: { purchaseSteps: number }) {
 
   return (
     <>
-     <NewAdressFrom createNewAddress={createNewAddress} setAddress={setAddress} setCreateNewAddress={setCreateNewAddress}/>
-      
-      <div
-        style={createNewPay ? { display: "block" } : { display: "none" }}
-        className="absolute left-[50%] top-[50%] z-10 h-[50%] w-[50%] translate-x-[-50%] translate-y-[-50%] rounded border border-black bg-white"
-      >
-        <p>Add a new payment method</p>
-        <input
-          type="text"
-          name="cardNumber"
-          placeholder="Card Number"
-          value={newPaymentMethod.cardNumber}
-          onChange={handleCardNumberChange}
-          maxLength={19}
-        />
-        <input
-          type="text"
-          name="expirationDate"
-          placeholder="Expiration Date"
-          value={newPaymentMethod.expirationDate}
-          onChange={handleInputChangeCard}
-        />
-        <input
-          type="text"
-          name="ownerName"
-          placeholder="Owner Name"
-          value={newPaymentMethod.ownerName}
-          onChange={handleInputChangeCard}
-        />
-        <button onClick={createPaymentMethod}>Add payment method</button>
-        <p onClick={() => setCreateNewPay(false)}>X</p>
-      </div>
-      <div className=" mt-2 flex flex-grow flex-row gap-4">
+      <NewCardForm
+        createNewPay={createNewPay}
+        getPaymentMethods={getPaymentMethods}
+        setCreateNewPay={setCreateNewPay}
+      />
+      <NewAdressFrom
+        createNewAddress={createNewAddress}
+        setAddress={setAddress}
+        setCreateNewAddress={setCreateNewAddress}
+      />
+
+      <div className="mt-2 flex flex-grow flex-row gap-4">
         <div
           id="processPurchase"
           className="flex w-[80%] flex-[3] translate-y-80 flex-col items-end opacity-0"
