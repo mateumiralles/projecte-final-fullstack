4de9c11eba9ae116d174e315cf6344dfc6bb6ca0
Idyos <javi.gauxachs@gmail.com>
2023-09-26 23:59:11 +0200

    ingles i pestanyes no necessaries fora

--- frontend/app/components/Navbar/UserProfile.tsx
+++ frontend/app/components/Navbar/UserProfile.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 export default function UserProfile(props: {setProfile: React.Dispatch<React.SetStateAction<any>>, logout: (id: number) => Promise<void>, user: any}){
 
@@ -6,25 +7,20 @@ export default function UserProfile(props: {setProfile: React.Dispatch<React.Set
     return(
         <div className="absolute right-0 top-full w-96 bg-white flex flex-col rounded border border-black cursor-default">
             <h1 className="mt-5 text-lg font-bold">
-                Hola, {props.user.name}
+                Hi, {props.user.name}
             </h1>
-            <div onClick={() => console.log("HOLAAAAAAAA")} className="p-5 flex flex-row justify-between cursor-pointer">
-                <p>Tus pedidos</p>
-                <p>Icon</p>
-            </div>
+            <Link href="/ordersPage">
             <div className="p-5 flex flex-row justify-between cursor-pointer">
-                <p>Tu lista de deseos</p>
+                <p>Your orders</p>
                 <p>Icon</p>
             </div>
-            <div className="p-5 flex flex-row justify-between cursor-pointer">
-                <p>Métodos de pago</p>
-                <p>Icon</p>
-            </div>
-            <div className="p-5 flex flex-row justify-between cursor-pointer">
-                <p>Direcciones</p>
-                <p>Icon</p>
-            </div>
-            
+            </Link>
+            <Link href="/wishlistPage">
+                <div className="p-5 flex flex-row justify-between cursor-pointer">
+                    <p>Your wishlist</p>
+                    <p>Icon</p>
+                </div>
+            </Link>
             <button className="mt-auto p-5 hover:text-red-500 border-t border-black cursor-pointer" onClick={() => props.logout(props.user.id)}>CERRAR SESIÓN</button>
         </div>
     )
