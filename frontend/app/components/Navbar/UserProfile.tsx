83f2a66f232ddb2625804316b21b7e88e98e875f
MateuMiralles <mateumiralles714@gmail.com>
2023-09-27 03:46:38 +0200

    -UserProfile Style

--- frontend/app/components/Navbar/UserProfile.tsx
+++ frontend/app/components/Navbar/UserProfile.tsx
@@ -1,27 +1,38 @@
-import axios from "axios";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-export default function UserProfile(props: {setProfile: React.Dispatch<React.SetStateAction<any>>, logout: (id: number) => Promise<void>, user: any}){
-
-
-    return(
-        <div className="absolute right-0 top-full w-96 bg-white flex flex-col rounded border border-black cursor-default">
-            <h1 className="mt-5 text-lg font-bold">
-                Hi, {props.user.name}
-            </h1>
-            <Link href="/ordersPage">
-            <div className="p-5 flex flex-row justify-between cursor-pointer">
-                <p>Your orders</p>
-                <p>Icon</p>
-            </div>
-            </Link>
-            <Link href="/wishlistPage">
-                <div className="p-5 flex flex-row justify-between cursor-pointer">
-                    <p>Your wishlist</p>
-                    <p>Icon</p>
-                </div>
-            </Link>
-            <button className="mt-auto p-5 hover:text-red-500 border-t border-black cursor-pointer" onClick={() => props.logout(props.user.id)}>CERRAR SESIÓN</button>
-        </div>
-    )
+export default function UserProfile(props: {
+  setProfile: React.Dispatch<React.SetStateAction<any>>;
+  logout: (id: number) => Promise<void>;
+  user: any;
+}) {
+  return (
+    <div className="absolute right-0 top-full flex w-96 cursor-default flex-col rounded border border-black bg-white">
+      <h1 className="my-5 text-lg font-bold">Hi, {props.user.name}</h1>
+      <div className="flex flex-col h-full gap-5 mb-5">
+      <Link href="/ordersPage">
+        <p
+          className="mx-5 flex cursor-pointer items-center justify-center rounded border border-black hover:bg-black p-4 hover:text-white transition duration-300 ease-in-out hover:scale-95"
+          title="Go to previous orders"
+        >
+          Order history
+        </p>
+      </Link>
+      <Link href="/wishlistPage">
+        <p
+          className="mx-5 flex cursor-pointer items-center justify-center rounded border border-black hover:bg-black p-4 hover:text-white transition duration-300 ease-in-out hover:scale-95"
+          title="Go to wishlist"
+        >
+          Wishlist
+        </p>
+      </Link>
+      </div>
+      <div
+        className="group mt-auto cursor-pointer rounded border-t border-black p-5 font-bold transition  duration-300 ease-in-out hover:bg-red-500 hover:text-white"
+        onClick={() => props.logout(props.user.id)}
+      >
+        <p className="transition duration-200 ease-in-out group-hover:scale-110">
+          Log Out
+        </p>
+      </div>
+    </div>
+  );
 }
