a0b68a7cca8d99d35cf7061ce59441547b08a5b1
MateuMiralles <mateumiralles714@gmail.com>
2023-09-28 10:59:33 +0200

    -Disseny ordersProductCard acabat

--- frontend/app/ordersPage/components/ordersProductCard.tsx
+++ frontend/app/ordersPage/components/ordersProductCard.tsx
@@ -18,46 +18,58 @@ export default function ProductCardOrders({
   orderObject,
   j,
 }: ProductCardOrdersProps) {
-  const [productData, setProductData] = useState<ProductInfo | undefined>(undefined);
-
+  const [productData, setProductData] = useState<ProductInfo | undefined>(
+    undefined,
+  );
 
   const getProductInfo = async () => {
-    try{
-      const product = await axios.get(`http://localhost:3333/api/productSummaries/${orderObject.productSummaryCode}`);
-      if(product.status===200){
-        setProductData({img: product.data.img, name: product.data.name, price: product.data.price});
+    try {
+      const product = await axios.get(
+        `http://localhost:3333/api/productSummaries/${orderObject.productSummaryCode}`,
+      );
+      if (product.status === 200) {
+        setProductData({
+          img: product.data.img,
+          name: product.data.name,
+          price: product.data.price,
+        });
       }
     } catch (error: any) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     getProductInfo();
-  }, [])
+  }, []);
 
   let productParent = orderObject.productSummaryCode.slice(0, -3);
   return (
-    <div
-      key={j}
-      className="flex min-w-[200px] flex-col items-center rounded border border-black bg-white p-5"
+    <Link
+      href={`/detailPage?productId=${orderObject.productSummaryCode}&productParent=${productParent}`}
+      className="cursor-pointer"
     >
-      <Link href={`/detailPage?productId=${orderObject.productSummaryCode}&productParent=${productParent}`}>
-        <p className="w-full text-left text-lg font-bold">
-          {productData?.name}{" "}
-          <span className="text-sm font-normal">x{orderObject.quantity}</span>
-        </p>
+      <div
+        key={j}
+        className="flex min-w-[200px] flex-col items-center rounded p-5 transition  duration-200 ease-in-out hover:scale-95 border hover:border-black hover:bg-white"
+      >
+        <div className="w-full flex flex-row content-center items-center">
+          <p className="text-lg font-bold">
+            {productData?.name.split(" ")[0]}
+          </p>
+          <p className="text-sm font-normal ml-2">{` x${orderObject.quantity}`}</p>
+        </div>
         <p className="w-full text-left text-xs">{`Cloth code: ${orderObject.productSummaryCode}`}</p>
 
-        <div className="h-80 w-full rounded bg-gray-300">
-          {productData!= undefined ?
-          <Image 
-          alt="Product Image"
-          src={productData?.img}
-          width={214}
-          height={320}
-          />
-          : null}
+        <div className="my-5 h-80 w-full rounded bg-gray-300">
+          {productData != undefined ? (
+            <Image
+              alt="Product Image"
+              src={productData?.img}
+              width={214}
+              height={320}
+            />
+          ) : null}
         </div>
         <div className="flex w-full flex-row justify-between">
           <div className="flex flex-row items-center ">
@@ -70,7 +82,7 @@ export default function ProductCardOrders({
           </div>
           <p className="text-sm">{productData?.price} €</p>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
